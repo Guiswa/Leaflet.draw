ce60dfab868192dab9ba03c07073e1b03891d33c
Aaron King <floodle@floodle.org>
2012-07-02 13:15:35 -0700

    Merge remote-tracking branch 'upstream/master'

--- src/SimpleShape.Draw.js
+++ src/SimpleShape.Draw.js
@@ -1,17 +1,6 @@
-L.Circle.Draw = L.Handler.Draw.extend({
-	options: {
-		shapeOptions: {
-			stroke: true,
-			color: '#f06eaa',
-			weight: 4,
-			opacity: 0.5,
-			fill: true,
-			fillColor: null, //same as color by default
-			fillOpacity: 0.2,
-			clickable: true
-		}
-	},
+L.SimpleShape = {};
 
+L.SimpleShape.Draw = L.Handler.Draw.extend({
 	addHooks: function () {
 		L.Handler.Draw.prototype.addHooks.call(this);
 		if (this._map) {
@@ -19,7 +8,7 @@ L.Circle.Draw = L.Handler.Draw.extend({
 			//TODO refactor: move cursor to styles
 			this._container.style.cursor = 'crosshair';
 
-			this._updateLabelText({ text: 'Click and drag to draw circle.' });
+			this._updateLabelText({ text: this._initialLabelText });
 
 			L.DomEvent
 				.addListener(this._container, 'mousedown', this._onMouseDown, this)
@@ -39,10 +28,10 @@ L.Circle.Draw = L.Handler.Draw.extend({
 				.removeListener(document, 'mousemove', this._onMouseMove)
 				.removeListener(document, 'mouseup', this._onMouseUp);
 
-			// If the circle element doesn't exist they must not have moved the mouse, so don't need to destroy/return
-			if (this._circ) {
-				this._map.removeLayer(this._circ);
-				delete this._circ;
+			// If the box element doesn't exist they must not have moved the mouse, so don't need to destroy/return
+			if (this._shape) {
+				this._map.removeLayer(this._shape);
+				delete this._shape;
 			}
 		}
 		this._isDrawing = false;
@@ -54,8 +43,6 @@ L.Circle.Draw = L.Handler.Draw.extend({
 		this._updateLabelText({ text: 'Release mouse to finish drawing.' });
 
 		this._startLatLng = this._map.mouseEventToLatLng(e);
-
-		this._startLayerPoint = this._map.mouseEventToLayerPoint(e);
 
 		L.DomEvent
 			.addListener(document, 'mouseup', this._onMouseUp, this)
@@ -70,22 +57,14 @@ L.Circle.Draw = L.Handler.Draw.extend({
 
 		if (this._isDrawing) {
 			this._updateLabelPosition(layerPoint);
-			if (!this._circ) {
-				this._circ = new L.Circle(this._startLatLng, this._startLatLng.distanceTo(latlng), this.options.shapeOptions);
-				this._map.addLayer(this._circ);
-			} else {
-				this._circ.setRadius(this._startLatLng.distanceTo(latlng));
-			}
+			this._drawShape(latlng);
 		}
 	},
 
 	_onMouseUp: function (e) {
 		this._endLatLng = this._map.mouseEventToLatLng(e);
-		
-		this._map.fire(
-			'draw:circle-created',
-			{ circ: new L.Circle(this._startLatLng, this._startLatLng.distanceTo(this._endLatLng), this.options.shapeOptions) }
-		);
+
+		this._fireCreatedEvent();
 		
 		this.disable();
 	}
